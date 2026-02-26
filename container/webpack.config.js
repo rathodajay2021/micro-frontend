ecf4a826e3b07fefe220eb25f06bf9f3b9c6b53c
Ajay Rathod <rathodajay2021@gmail.com>
2026-02-26 08:31:15 +0530

    feat: added cart & integrate with container

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,14 +1,21 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { mode, plugins } = require('../products/webpack.config')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
-    mode: 'development',
-    devServer: {
-        port: 8080
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
-    ]
-}
+  mode: "development",
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+        name: 'container',
+        remotes: {
+            products: 'products@http://localhost:8081/remoteEntry.js',
+            cart: 'cart@http://localhost:8082/remoteEntry.js'
+        }
+    })
+  ],
+};
